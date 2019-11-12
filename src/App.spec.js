43b8d42cca01e5e026859c022dca450f7a21e45c
Kelve Aragão <kelvearagao@gmail.com>
2019-11-11 23:10:54 -0300

    Config test to render with jest and babel

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,10 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+import App from "./App"
+
 describe("App", () => {
-  it("Runs and passes", () => {
-    expect(true).toBe(true)
+  it("Renders without errors", () => {
+    render(<App />)
   })
 })
