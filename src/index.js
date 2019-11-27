351c75febab54106f4fa70268d3865805650b2eb
Kelve Aragão <kelvearagao@gmail.com>
2019-11-26 23:56:08 -0200

    Adiciona react axe

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import './styles.css'
 import DefaultErrorBoundary from './DefaultErrorBoundary'
 
 if (process.env.NODE_ENV === 'development') {
-    const axe = require('react-axe')
-    axe(React, ReactDOM, 5000)
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 5000)
 }
 
 ReactDOM.render(
